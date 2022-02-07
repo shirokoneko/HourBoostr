9efc092ac1cdda63c1f50210012e216991ffd430
Ⱨ₳₱₱Ɏ₣ɆɆ₮ <Stephenbuisness@outlook.com>
2022-02-07 22:30:14 +0000

    Updated Splash Screen

--- Source/HourBoostr-Beta/Ui/SplashScreen.Designer.cs
+++ Source/HourBoostr-Beta/Ui/SplashScreen.Designer.cs
@@ -54,7 +54,7 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Image = global::HourBoostr_Beta.Properties.Resources.SplashScreen;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.Location = new System.Drawing.Point(0, 1);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(870, 481);
